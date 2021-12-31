30b2c1d445febfd1e2dfd17ef9df28aa7f0e4286
thinhhja2001 <81420944+thinhhja2001@users.noreply.github.com>
2021-12-31 20:26:31 +0700

    update add email

--- Booking/front-end/src/components/main/contact.jsx
+++ Booking/front-end/src/components/main/contact.jsx
@@ -6,68 +6,101 @@ import '../../pages/home.css';
 import '../main.css';
 import '../reponsive.css';
 import Img_pic from './../../assests/traveler.png';
-class Contact extends Component {
-	render() {
-		return (
-			<Fragment>
-				<section class="contact">
-					<div class="container">
-						<h5 class="section-head">
-							<span class="heading">Contact</span>
-							<span class="sub-heading">Get in touch with us</span>
-						</h5>
-						<div class="contact-content">
-							<div class="traveler-wrap">
-								<img src={Img_pic} alt=""></img>
-							</div>
-							<form action="" class="form contact-form">
-								<div class="input-group-wrap">
-									<div class="input-group">
-										<input
-											type="text"
-											class="input"
-											placeholder="Name"
-											required
-										></input>
-										<span class="bar"></span>
-									</div>
-									<div class="input-group">
-										<input
-											type="email"
-											class="input"
-											placeholder="E-mail"
-											required
-										></input>
-										<span class="bar"></span>
-									</div>
-								</div>
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+
+toast.configure();
+function Contact() {
+	const [name, setName] = useState();
+	const [mail, setMail] = useState();
+	const [subject, setSubject] = useState();
+	const [content, setContent] = useState();
+	const handleClick = () => {
+		if (name && mail) {
+			axios.post(`${process.env.REACT_APP_API_URL}/api/v1/contact/`, {
+				name: name,
+				from: mail,
+				subject: subject,
+				content: content
+			}).then(
+				toast.success('Email sent, we will contact you as soon as possible', {
+					position: 'top-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+				})
+			)
+		}
+
+	}
+	return (
+		<Fragment>
+			<section class="contact">
+				<div class="container">
+					<h5 class="section-head">
+						<span class="heading">Contact</span>
+						<span class="sub-heading">Get in touch with us</span>
+					</h5>
+					<div class="contact-content">
+						<div class="traveler-wrap">
+							<img src={Img_pic} alt=""></img>
+						</div>
+						<div class="form contact-form">
+							<div class="input-group-wrap">
 								<div class="input-group">
 									<input
+										onChange={(e) => setName(e.target.value)}
 										type="text"
 										class="input"
-										placeholder="Subject"
+										placeholder="Name"
+										required
 									></input>
 									<span class="bar"></span>
 								</div>
 								<div class="input-group">
-									<textarea
+									<input
+										onChange={(e) => setMail(e.target.value)}
+										type="email"
 										class="input"
-										cols="30"
-										rows="8"
-										placeholder="Content"
-									></textarea>
+										placeholder="E-mail"
+										required
+									></input>
 									<span class="bar"></span>
 								</div>
-								<button type="submit" class="btn form-btn btn-orange">
-									Send message
-								</button>
-							</form>
+							</div>
+							<div class="input-group">
+								<input
+									onChange={(e) => setSubject(e.target.value)}
+									type="text"
+									class="input"
+									placeholder="Subject"
+								></input>
+								<span class="bar"></span>
+							</div>
+							<div class="input-group">
+								<textarea
+									onChange={(e) => setContent(e.target.value)}
+									class="input"
+									cols="30"
+									rows="8"
+									placeholder="Content"
+								></textarea>
+								<span class="bar"></span>
+							</div>
+							<button onClick={handleClick} class="btn form-btn btn-orange">
+								Send message
+							</button>
 						</div>
 					</div>
-				</section>
-			</Fragment>
-		);
-	}
+				</div>
+			</section>
+		</Fragment>
+	);
 }
 
 export default Contact;
